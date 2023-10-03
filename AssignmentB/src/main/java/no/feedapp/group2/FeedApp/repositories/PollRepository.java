fa36b9930cc09436825d8082bd0f9a8bf69ec43e
mikkelgunhildsbu <mikkelgunhildsbu@gmail.com>
2023-10-03 13:11:29 +0200

    Poll

--- AssignmentB/src/main/java/no/feedapp/group2/FeedApp/repositories/PollRepository.java
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/repositories/PollRepository.java
@@ -1,7 +1,11 @@
 package no.feedapp.group2.FeedApp.repositories;
 
+import no.feedapp.group2.FeedApp.domain.Customer;
 import no.feedapp.group2.FeedApp.domain.Poll;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PollRepository extends CrudRepository<Poll, Long> {
+    Poll findByPollId(long userId);
+
+    Poll deletePollById(long userId);
 }
