e4a87fb3391fdf148a8728bc8d3fcf20f2e44e97
Marius Hatland <marius.hatland@student.uib.no>
2023-10-03 13:12:24 +0200

    wip

--- AssignmentB/src/main/java/no/feedapp/group2/FeedApp/repositories/PollRepository.java
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/repositories/PollRepository.java
@@ -3,5 +3,7 @@ package no.feedapp.group2.FeedApp.repositories;
 import no.feedapp.group2.FeedApp.domain.Poll;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface PollRepository extends CrudRepository<Poll, Long> {
 }
