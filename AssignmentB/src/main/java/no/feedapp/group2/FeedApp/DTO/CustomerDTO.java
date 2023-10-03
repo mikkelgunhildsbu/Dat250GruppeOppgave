c67a2088ae8301e0631d33bb69689e5cb80c3555
Marius Hatland <marius.hatland@student.uib.no>
2023-10-03 14:29:07 +0200

    added customer DTO conversion

--- AssignmentB/src/main/java/no/feedapp/group2/FeedApp/DTO/CustomerDTO.java
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/DTO/CustomerDTO.java
@@ -1,6 +1,22 @@
 package no.feedapp.group2.FeedApp.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+import no.feedapp.group2.FeedApp.domain.Customer;
+
+@Setter
+@Getter
 public class CustomerDTO {
+    private Long userId;
     private String userName;
     private String email;
+
+    public static CustomerDTO ConvertToDTO(Customer customer){
+        var dto = new CustomerDTO();
+        dto.setUserId(customer.getUserId());
+        dto.setEmail(customer.getEmail());
+        dto.setUserName(customer.getUserName());
+
+        return dto;
+    }
 }
--- AssignmentB/src/main/java/no/feedapp/group2/FeedApp/controllers/CustomerController.java
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/controllers/CustomerController.java
@@ -1,5 +1,6 @@
 package no.feedapp.group2.FeedApp.controllers;
 
+import no.feedapp.group2.FeedApp.DTO.CustomerDTO;
 import no.feedapp.group2.FeedApp.DTO.CustomerUpdateDTO;
 import no.feedapp.group2.FeedApp.domain.Customer;
 import no.feedapp.group2.FeedApp.repositories.CustomerRepository;
@@ -19,17 +20,17 @@ public class CustomerController {
     }
 
     @PostMapping("/customer")
-    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer){
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody Customer customer){
         customerRepository.save(customer);
-        return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+        return ResponseEntity.status(HttpStatus.CREATED).body(CustomerDTO.ConvertToDTO(customer));
     }
 
     @GetMapping("/customer/{id}")
-    public ResponseEntity<Customer> getCustomerById(@PathVariable String id){
+    public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable String id){
         Customer customer = customerRepository.findByUserId(Long.parseLong(id));
 
         if (customer != null){
-            return ResponseEntity.ok(customer);
+            return ResponseEntity.ok(CustomerDTO.ConvertToDTO(customer));
         }
         else{
             return ResponseEntity.notFound().build();
@@ -37,7 +38,7 @@ public class CustomerController {
     }
 
     @PutMapping("/customer/{id}")
-    public ResponseEntity<Customer> updateCustomer(@PathVariable String id, @RequestBody CustomerUpdateDTO updatedCustomer){
+    public ResponseEntity<CustomerUpdateDTO> updateCustomer(@PathVariable String id, @RequestBody CustomerUpdateDTO updatedCustomer){
         try{
             Customer existingCustomer = customerRepository.findByUserId(Long.parseLong(id));
 
@@ -57,7 +58,7 @@ public class CustomerController {
 
             customerRepository.save(existingCustomer);
 
-            return ResponseEntity.ok(existingCustomer);
+            return ResponseEntity.ok(updatedCustomer);
 
         } catch (Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
