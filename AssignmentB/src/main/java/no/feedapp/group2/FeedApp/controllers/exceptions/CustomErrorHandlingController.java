a90bf75441535cd7c09acf0190ffa26ab906384a
Marius Hatland <marius.hatland@student.uib.no>
2023-10-23 10:16:08 +0200

    Added global error handling controller

---
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/controllers/exceptions/CustomErrorHandlingController.java
@@ -0,0 +1,53 @@
+package no.feedapp.group2.FeedApp.controllers.exceptions;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+class CustomErrorHandlingController {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    CustomErrorResponse onConstraintValidationException(ConstraintViolationException e) {
+        CustomErrorResponse error = new CustomErrorResponse();
+        e.getConstraintViolations().forEach(violation -> error.getErrors().add(
+                new CustomError(violation.getPropertyPath().toString(), violation.getMessage())));
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    CustomErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        CustomErrorResponse error = new CustomErrorResponse();
+        e.getBindingResult().getFieldErrors().forEach(fieldError -> error.getErrors().add(
+                new CustomError(fieldError.getField(), fieldError.getDefaultMessage())));
+        return error;
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    CustomErrorResponse onIllegalArgumentException(IllegalArgumentException e) {
+        CustomErrorResponse error = new CustomErrorResponse();
+        error.getErrors().add(
+                new CustomError("Illegal argument", e.getMessage()));
+        return error;
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    CustomErrorResponse onCustomerNotFoundException(CustomerNotFoundException e) {
+        CustomErrorResponse error = new CustomErrorResponse();
+        error.getErrors().add(
+                new CustomError("Customer not found", e.getMessage()));
+        return error;
+    }
+}
