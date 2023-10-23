00df509532199352e61c2c6d9824b87e0ef03130
Marius Hatland <marius.hatland@student.uib.no>
2023-10-23 10:12:44 +0200

    Added validation to Customer

--- AssignmentB/src/main/java/no/feedapp/group2/FeedApp/domain/Customer.java
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/domain/Customer.java
@@ -2,6 +2,9 @@ package no.feedapp.group2.FeedApp.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,34 +15,32 @@ import java.util.Collection;
 @Setter
 public class Customer {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userId;
 
+    @NotBlank
     private String userName;
 
+    @Email
     private String email;
 
+    @NotBlank
     private String password;
 
+    @Valid
     private Role role;
 
     @OneToMany(mappedBy = "user")
     @JsonIgnore
     private Collection<Poll> polls;
 
-    protected Customer(){}
+    protected Customer() {
+    }
 
-    public Customer(String userName, String email, String password, Role role){
+    public Customer(String userName, String email, String password, Role role) {
         this.userName = userName;
         this.email = email;
         this.password = password;
         this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%d, userName='%s', email='%s']",
-                userId, userName, email);
     }
 }
