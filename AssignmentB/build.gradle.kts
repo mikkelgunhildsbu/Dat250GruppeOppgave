025c2e426055d9c2262a91de0f1d909ddabbadaa
Marius Hatland <marius.hatland@student.uib.no>
2023-11-09 12:06:16 +0100

    Added dependencies

--- AssignmentB/build.gradle.kts
+++ AssignmentB/build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     testImplementation("org.springframework.security:spring-security-test")
     implementation("io.github.cdimascio:dotenv-java:3.0.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
 
 tasks.withType<Test> {
