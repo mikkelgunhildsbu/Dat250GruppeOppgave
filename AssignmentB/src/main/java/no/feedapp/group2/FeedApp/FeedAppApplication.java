441dbaf4abf0d74a36c155d56e30afc106cc6568
Marius Hatland <marius.hatland@student.uib.no>
2023-11-14 15:47:45 +0100

    Added retry mechanism

--- AssignmentB/src/main/java/no/feedapp/group2/FeedApp/FeedAppApplication.java
+++ AssignmentB/src/main/java/no/feedapp/group2/FeedApp/FeedAppApplication.java
@@ -4,11 +4,26 @@ import no.feedapp.group2.FeedApp.rabbitMQ.Subscriber;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static java.lang.Thread.sleep;
+
 
 @SpringBootApplication
 public class FeedAppApplication {
     public static void main(String[] args) throws Exception {
-        new Subscriber().Subscriber();
+        var maxRetryCount = 10;
+        var retryCount = 0;
+
         SpringApplication.run(FeedAppApplication.class, args);
+
+        while (retryCount < maxRetryCount)
+            try {
+                new Subscriber().Subscriber();
+                break;
+            } catch (Exception e) {
+                System.out.println("Error: " + e);
+                System.out.println("Failed to connect to RabbitMQ, retrying in 5 seconds...");
+                retryCount++;
+                sleep(5000);
+            }
     }
 }
